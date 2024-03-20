f9d3e3fa0f3796a6497d5b5ae1e27ba352a4098b
Runze_Linux <runze.wang02@outlook.com>
2024-03-21 00:00:05 +0800

    branch Lab0

---
+++ build/apps/CMakeFiles/buftest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buftest.dir/buftest.cc.o"
+  "CMakeFiles/buftest.dir/buftest.cc.o.d"
+  "buftest"
+  "buftest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buftest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
