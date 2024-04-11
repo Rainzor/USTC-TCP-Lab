b85e49b1f97ee779ab4b711e6790b69ddb3eef5c
Runze_Linux <runze.wang02@outlook.com>
2024-04-11 19:12:26 +0800

    Init Lab6

--- tests/webget_t.sh
+++ tests/webget_t.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 /headless/sponge/tap.sh start
-#service apache2 start
-#echo "QWx0NhMPkoM/bJr/ohvHXlviFhOyYrYb+qqdOnwLYo4"  > /var/www/html/xyzzy
-#WEB_HASH=`./apps/webget 169.254.10.1 /xyzzy | tee /dev/stderr | tail -n 1`
-WEB_HASH=`./apps/webget cscourse.ustc.edu.cn /index.html|tee /dev/stderr|tail -n 1`
-#CORRECT_HASH="QWx0NhMPkoM/bJr/ohvHXlviFhOyYrYb+qqdOnwLYo4"
-CORRECT_HASH="</body></html>"
+service apache2 start
+echo "QWx0NhMPkoM/bJr/ohvHXlviFhOyYrYb+qqdOnwLYo4"  > /var/www/html/xyzzy
+WEB_HASH=`./apps/webget 169.254.10.1 /xyzzy | tee /dev/stderr | tail -n 1`
+CORRECT_HASH="QWx0NhMPkoM/bJr/ohvHXlviFhOyYrYb+qqdOnwLYo4"
 
 if [ "${WEB_HASH}" != "${CORRECT_HASH}" ]; then
     echo ERROR: webget returned output that did not match the test\'s expectations
